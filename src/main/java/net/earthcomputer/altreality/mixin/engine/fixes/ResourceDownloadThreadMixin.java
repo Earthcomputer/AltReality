8233d5130e8f2af56961e324985b82db09fd2868
Joe <burtonjae@hotmail.co.uk>
2021-06-22 21:13:42 +0100

    Make sure to load vanilla sounds

--- src/main/java/net/earthcomputer/altreality/mixin/engine/fixes/ResourceDownloadThreadMixin.java
+++ src/main/java/net/earthcomputer/altreality/mixin/engine/fixes/ResourceDownloadThreadMixin.java
@@ -4,15 +4,19 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.util.ResourceDownloadThread;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ResourceDownloadThread.class)
-public class ResourceDownloadThreadMixin {
+public abstract class ResourceDownloadThreadMixin {
+    @Shadow public abstract void method_107();
+
     @Inject(method = "run", at = @At("HEAD"), cancellable = true, remap = false)
     private void onRun(CallbackInfo ci) {
+        method_107();
         ci.cancel();
     }
 }
